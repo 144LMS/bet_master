1262924a10a72e9631da2bfdb0c24da441c9d013
Ilya Dr <PR23232@yandex.ru>
2025-02-22 12:47:28 +0300

    Creating User, Bet, and Admin structures

---
+++ api/bet/bet.go
@@ -0,0 +1,25 @@
+package bet
+
+type Bet struct {
+	IDBet      int     `json:"id_bet"`
+	FirstTeam  string  `json:"first_team"`
+	SecondTeam string  `json:"second_team"`
+	Winner     string  `json:"winner"`
+	Total      float64 `json:"total"`
+}
+
+type UpdateBetRequest struct {
+	IDBet      int     `json:"id_bet"`
+	FirstTeam  string  `json:"first_team"`
+	SecondTeam string  `json:"second_team"`
+	Total      float64 `json:"total"`
+}
+
+type WinnerBetRequest struct {
+	IDBet  int    `json:"id_bet"`
+	Winner string `json:"winner"`
+}
+
+type DeleteBetRequest struct {
+	IDBet int `json:"id_bet"`
+}
